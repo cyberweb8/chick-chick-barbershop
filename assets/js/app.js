f11d8dc42b78e7343e629818cd4e26f709248d6e
cyberweb8 <abdumalikshareef@gmail.com>
2023-01-31 00:30:30 +0500

    feat: add slider functionality

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+const addPreload = (elem) => {
+  elem.classList.add('preload');
+};
+
+const removePreload = (elem) => {
+  elem.classList.remove('preload');
+};
+
+const startSlider = () => {
+  const sliderItems = document.querySelectorAll('.slider__item');
+  // console.log('sliderItems: ', sliderItems);
+  const sliderList = document.querySelector('.slider__list');
+  const btnPrevSlider = document.querySelector('.slider__arrow--left');
+  const btnNextSlider = document.querySelector('.slider__arrow--right');
+
+  let activeSlide = 1;
+  let position = 0;
+
+  const nextSlider = () => {
+    sliderItems[activeSlide].classList.remove('slider__item_active');
+    let position = -sliderItems[0].clientWidth * activeSlide;
+    sliderList.style.transform = `translate(${position}px)`;
+    activeSlide++;
+    sliderItems[activeSlide].classList.add('slider__item_active');
+  };
+
+  const prevSlider = () => {
+    sliderItems[activeSlide].classList.remove('slider__item_active');
+    let position = -sliderItems[0].clientWidth * (activeSlide - 2);
+    sliderList.style.transform = `translate(${position}px)`;
+    activeSlide--;
+    sliderItems[activeSlide].classList.add('slider__item_active');
+  };
+
+  btnPrevSlider.addEventListener('click', prevSlider);
+  btnNextSlider.addEventListener('click', nextSlider);
+};
+const initSlider = () => {
+  const slider = document.querySelector('.slider');
+  const sliderContainer = document.querySelector('.slider__container');
+
+  sliderContainer.style.display = 'none';
+  addPreload(slider);
+  window.addEventListener('load', () => {
+    sliderContainer.style.display = '';
+    removePreload(slider);
+    startSlider(slider);
+  });
+};
+
+window.addEventListener('DOMContentLoaded', initSlider);
